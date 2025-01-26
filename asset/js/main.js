3500dfcc2ff63d9b80acf2d8ed5d301aa07d2f7f
edcfirm <edcfirm2023@gmail.com>
2025-01-26 08:02:06 -0500

    Update main.js

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,7 @@
+// Existing Carousel Initialization
 $('#myCarousel').carousel({
     interval: 5000
-})
+});
 
 $('#myCarousel .carousel-item').each(function() {
     var minPerSlide = 4;
@@ -19,10 +20,21 @@ $('#myCarousel .carousel-item').each(function() {
         next.children(':first-child').clone().appendTo($(this));
     }
 });
+
+// Mixitup Initialization
 var containerEl = document.querySelector('[data-ref~="mixitup-container"]');
 
-    var mixer = mixitup(containerEl, {
+var mixer = mixitup(containerEl, {
     selectors: {
         target: '[data-ref~="mixitup-target"]'
     }
+});
+
+// Highlight Active Navigation Button
+$('.main-menu ul li a').on('click', function() {
+    // Remove active class from all links
+    $('.main-menu ul li a').removeClass('active');
+
+    // Add active class to the clicked link
+    $(this).addClass('active');
 });
